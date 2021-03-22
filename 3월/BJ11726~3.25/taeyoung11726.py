32e43b947801dcb5f05a92d3f4425e0fb6bb37b6
taeyoung lee <taeyounglee93@gmail.com>
2021-03-22 15:44:32 +0900

    3.22

--- 3월/BJ11726~3.25/taeyoung11726.py
+++ 3월/BJ11726~3.25/taeyoung11726.py
@@ -1,13 +1,11 @@
-#푸는중 
 x = int(input())
 
 dp = [0] * (1001)
 dp[1] = 1
 dp[2] = 2
-for i in range(3,x+2,2):
-    dp[i] = dp[i-1] + dp[i-2] + 1
-    dp[i-1] = dp[i-2] + 1
+for i in range(3,x+1):    
+    dp[i] = dp[i-1] + dp[i-2]
 
 
-print(dp[:x])
-# print(dp[x])
+# print(dp[:x+1])
+print(dp[x]%10007)
