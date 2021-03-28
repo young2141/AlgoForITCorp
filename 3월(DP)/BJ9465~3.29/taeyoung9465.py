77c8209f27b0ad4c6f7081db5508e0efee7ac36f
taeyoung lee <taeyounglee93@gmail.com>
2021-03-28 14:56:36 +0900

    🤔

--- 3월(DP)/BJ9465~3.29/taeyoung9465.py
+++ 3월(DP)/BJ9465~3.29/taeyoung9465.py
@@ -1,15 +1,18 @@
+from pprint import pprint 
+# 푸는 중
 T = int(input())
 
 for _ in range(T):
     n = int(input())
-    dp = [[0] * n]*2
-    board = [[0]*(n+2)]
-    board.append([0] + [int(x) for x in input().split()]+[0])
-    board.append([0] + [int(x) for x in input().split()]+[0])
-    board.append([0]*(n+2))
+    dp = [[0] * (n+3)]*2
+    board = [[int(x) for x in input().split()]for y in range(2)]
+    board[0] += [0,0]
+    board[1] += [0,0]
+    
+    dp[0][0],dp[0][1],dp[1][0],dp[1][1] = board[0][0],board[0][1],board[1][0],board[1][1]
+    
+    for x in range(2, n+2):
+        dp[0][x] = max(dp[0][x-2], dp[1][x-1]) + board[0][x]
+        dp[1][x] = max(dp[1][x-2],dp[0][x-1]) + board[1][x]
 
-    dp[0][0] = board[1][1]
-    dp[0][1] = board[2][3]
-    for i in range(1, n+1):
-        for j in range(2):
-            dp[i][j] = max(dp[i][j-2], dp[i][j-3])
+    pprint(dp)
